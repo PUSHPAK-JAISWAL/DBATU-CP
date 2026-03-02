12f66e00223330e5b38d9b18762d1e3e1f4a588c
Pushpak-Jaiswal <pushpakmjaiswal@gmail.com>
2026-03-02 11:43:52 +0530

    mine ts

---
+++ MineSweeper/solution.ts
@@ -0,0 +1,63 @@
+import * as fs from "fs";
+
+function solve() {
+    const input = fs.readFileSync(0,"utf8").trim().split("\n");
+    let idx = 0;
+    let field = 1;
+
+    const output:string[] = [];
+
+    while(true) {
+        const[nStr,mStr] = input[idx++].split(" ");
+        const n = Number(nStr);
+        const m = Number(mStr);
+
+        if(n===0 && m===0) break;
+
+        const grid: string[] = [];
+        for(let i = 0;i<n;i++) {
+            grid.push(input[idx++]);
+        }
+
+        const result:string[] = [];
+
+        for(let i = 0;i<n;i++) {
+            let row = "";
+            for(let j = 0;j<m;j++) {
+                if(grid[i][j]==="*") {
+                    row += "*";
+                } else {
+                    let count = 0;
+
+                    for(let dx = -1;dx<=1;dx++) {
+                        for(let dy=-1;dy<=1;dy++) {
+                            if(dx===0 && dy===0) continue;
+
+                            const ni = i+dx;
+                            const nj = j+dy;
+
+                            if(
+                                ni>=0 && ni<n &&
+                                nj>=0 && nj<m &&
+                                grid[ni][nj] === "*"
+                            ) {
+                                count++;
+                            }
+                        }
+                    }
+                    row += count.toString();
+                }
+            }
+            result.push(row);
+        }
+
+        if(field>1) output.push("");
+        output.push(`Field #${field}:`);
+        output.push(...result)
+
+        field++;
+    }
+    console.log(output.join("\n"));
+}
+
+solve();
